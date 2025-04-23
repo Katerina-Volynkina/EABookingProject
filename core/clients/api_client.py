bed0288be14f258ee5e2ae00589f5787eff8855c
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2025-04-23 16:09:56 +0300

    удалила print

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -94,8 +94,7 @@ class APIClient:
             response.raise_for_status()
         with allure.step('Checking status code'):
             assert response.status_code == 200, f'Expected status 201, but got {response.status_code}'
-            print(f"Create booking status code: {response.status_code}")
-            print(f"Response body: {response.text}")
+
         return response.json()
 
     def get_booking_ids(self, params=None):
